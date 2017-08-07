d13f7d7e6cce4f58eedd5ab32f33eeaecaaf773f
SeoInbo <seo4234@flerogames.com>
2017-08-07 18:33:12 +0900

    no comment

--- src/app/scrap/scrap.component.ts
+++ src/app/scrap/scrap.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+import { DataService } from '../services/data.service';
+
 @Component({
   selector: '.scrap',
   templateUrl: './scrap.component.html'
 })
 
 export class ScrapComponent {
+    protected scraps: any[] = [];
+
+    private dbOffset: number = 0;
+    private dbLimit: number = 4;
+    private boxCount: number = 0;
+
+    constructor(private dataService: DataService) {
+        this.loadDatabase();
+    }
+
+    loadDatabase(limit: number = this.dbLimit) {
+        let data = this.dataService.get("scrap", this.dbOffset, limit);
+        for (let row of data) {
+            this.addItem(row);
+        }
+    }
+
+    addItem(item) {
+        this.scraps.push({
+            id: item.id,
+            type: item.type,
+            title: item.title,
+            desc: item.desc,
+            srctit: item.srctit,
+            source: item.source,
+            image: item.image,
+            link: item.link
+        });
+        this.boxCount++;
+        this.dbOffset++;
+    }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -53,6 +53,73 @@ export class DataService {
                 images: "https://www.dothome.co.kr/static/images/main/main-slide3.jpg",
                 time: 1501513200 // 20170801
             }
+        ],
+        "scrap": [
+            {
+                id: 1,
+                type: "news",
+                title: "수확의 기쁨을 느끼고 가을 농사를 준비하는 달",
+                desc: "매년 8월 상순은 가마솥 더위가 절정을 이룬다. 중순까지 막바지 무더위가 이어지다가 하순 께가 되면 슬그머니 꼬리를 내린다. 예비 귀농·귀촌인이나 초보 귀농·귀촌인들이 알아두어야 할 ‘8월의 전원생활’은 무엇일까?",
+                srctit: "두산백과사전",
+                source: "www.naver.com",
+                image: "https://s.pstatic.net/static/www/mobile/edit/2017/0626/cropImg_166x108_98923112182913433.png",
+                link: "about:blank"
+
+            }, {
+                id: 2,
+                type: "blog",
+                title: "제주도 한 외딴 농장 파티에 CEO들이 모여드는 까닭",
+                desc: "우리나라 사람들에게 제주도는 각별한 관광지다. 철저한 계획을 세워 값진 시간을 쪼개 방문하는 곳이기도 하고, 때로는 갑자기 어디론가 훌쩍 떠나고 싶을 때 큰 부담 없이 찾는 장소기도 하다. 한국인이 아끼는 명소, 제주도로 삶의 터전을 옮긴 CEO들이 한곳에 모인 에코파티 현장을 찾았다.",
+                srctit: "네이버",
+                source: "www.naver.com",
+                image: "https://s.pstatic.net/static/www/mobile/edit/2017/0804/cropImg_339x222_102278677778141503.png",
+                link: "about:blank"
+            }, {
+                id: 3,
+                type: "dic",
+                title: "충청남도 논산시 명재고택",
+                desc: "'익다'라는 말은 열매나 씨가 여물 때, 고기나 곡식이 열을 받아 맛이 변했을 때, 혹은 술이나 장이 맛이 들 때 사용한다. 다른 의미의 '익다'는 자주 경험해 서투르지 않고 익숙할 때 사용한다. 여름의 뜨거운 햇볕 아래 익어가는 장독대들이 보인다. 우리 또한 서툴렀던 봄을 지나 무더운 여름 아래 익어가고 있는지도 모른다.",
+                srctit: "조선일보",
+                source: "www.naver.com",
+                image: "https://s.pstatic.net/static/www/mobile/edit/2017/0804/cropImg_750x352_102272018524634867.jpeg",
+                link: "about:blank"
+            }, {
+                id: 4,
+                type: "etc",
+                title: "김매기와 물관리만 잘하면 누구나 할 수 있어요",
+                desc: "우리나라 사람들에게 제주도는 각별한 관광지다. 철저한 계획을 세워 값진 시간을 쪼개 방문하는 곳이기도 하고, 때로는 갑자기 어디론가 훌쩍 떠나고 싶을 때 큰 부담 없이 찾는 장소기도 하다. 한국인이 아끼는 명소, 제주도로 삶의 터전을 옮긴 CEO들이 한곳에 모인 에코파티 현장을 찾았다.",
+                srctit: "카카오스토리",
+                source: "www.naver.com",
+                image: "https://s.pstatic.net/static/www/mobile/edit/2017/0804/cropImg_339x222_102278952269194915.png",
+                link: "about:blank"
+            }, {
+                id: 5,
+                type: "news",
+                title: "1㎏에 1000만원 하는 제주 양식 ‘해마’, 중국 노린다",
+                desc: "우리나라 사람들에게 제주도는 각별한 관광지다. 철저한 계획을 세워 값진 시간을 쪼개 방문하는 곳이기도 하고, 때로는 갑자기 어디론가 훌쩍 떠나고 싶을 때 큰 부담 없이 찾는 장소기도 하다. 한국인이 아끼는 명소, 제주도로 삶의 터전을 옮긴 CEO들이 한곳에 모인 에코파티 현장을 찾았다.",
+                srctit: "브런치 블로그",
+                source: "www.naver.com",
+                image: "https://s.pstatic.net/imgnews/image/origin/025/2017/07/05/2732988.jpg",
+                link: "about:blank"
+            }, {
+                id: 6,
+                type: "news",
+                title: "사라진 토종 단감, 지금 먹는 건 일본에서 온 품종",
+                desc: "우리나라 사람들에게 제주도는 각별한 관광지다. 철저한 계획을 세워 값진 시간을 쪼개 방문하는 곳이기도 하고, 때로는 갑자기 어디론가 훌쩍 떠나고 싶을 때 큰 부담 없이 찾는 장소기도 하다. 한국인이 아끼는 명소, 제주도로 삶의 터전을 옮긴 CEO들이 한곳에 모인 에코파티 현장을 찾았다.",
+                srctit: "네이버",
+                source: "www.naver.com",
+                image: "https://s.pstatic.net/static/www/mobile/edit/2017/0803/cropImg_166x108_102212231940461773.jpeg",
+                link: "about:blank"
+            }, {
+                id: 7,
+                type: "news",
+                title: "제주도 한 외딴 농장 파티에 CEO들이 모여드는 까닭",
+                desc: "우리나라 사람들에게 제주도는 각별한 관광지다. 철저한 계획을 세워 값진 시간을 쪼개 방문하는 곳이기도 하고, 때로는 갑자기 어디론가 훌쩍 떠나고 싶을 때 큰 부담 없이 찾는 장소기도 하다. 한국인이 아끼는 명소, 제주도로 삶의 터전을 옮긴 CEO들이 한곳에 모인 에코파티 현장을 찾았다.",
+                srctit: "다음 뉴스",
+                source: "www.naver.com",
+                image: "https://s.pstatic.net/static/www/mobile/edit/2017/0804/cropImg_339x222_102278677778141503.png",
+                link: "about:blank"
+            }
         ]
     };
 
